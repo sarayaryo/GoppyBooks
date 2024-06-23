7caa1b6795fe8039f34a838292afaadb6548052a
abetaichi <taichi.sept.28@gmail.com>
2024-06-23 16:16:13 +0900

    ローカルcsvに対する検索機能を実装

--- book_app.py
+++ book_app.py
@@ -1,27 +1,21 @@
-from flask import Flask, render_template
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
-# Googleスプレッドシートの認証
-def authenticate_google_docs():
-    scope = [
-        'https://spreadsheets.google.com/feeds',
-        'https://www.googleapis.com/auth/drive'
-    ]
-    # サービスアカウントのJSONファイルキーのパスを指定
-    creds = ServiceAccountCredentials.from_json_keyfile_name('goto-books-project-f4eb1a3c3a60.json', scope)
-    client = gspread.authorize(creds)
-    return client
+# CSVファイルの読み込み
+df = pd.read_csv('dataset.csv')
 
 @app.route('/')
 def index():
-    client = authenticate_google_docs()
-    sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1K9aJxB0sjrETjSpE297mniL_l1IbQtZWxiNL-uvFB70/edit#gid=0').sheet1
-    books = sheet.get_all_records()  # 全データを辞書のリストとして取得
-    return render_template('layout.html', books=books)
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['query']
+    results = df[df['description'].str.contains(query, na=False, case=False)]
+    indices = results.index.tolist()
+    return render_template('results.html', indices=indices)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0') 
-    #app.run(debug=True) 
+    app.run(debug=True, host='0.0.0.0')
